3f44cbc1b0255e7d95edbcbe2a121afc0e1e27c8
Brandon Benson <brandonbenson09@gmail.com>
2022-07-09 14:03:12 -0400

    added post model

---
+++ models/index.js
@@ -0,0 +1,9 @@
+const User = require("./User");
+const Post = require("./Post");
+const Comment = require("./Comment");
+
+User.hasMany({});
+
+User.hasMany({});
+
+module.exports = { User, Post, Comment };
