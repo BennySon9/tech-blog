76d5663ea83b8e74a231beef203dd920c039b21f
Brandon Benson <brandonbenson09@gmail.com>
2022-07-10 18:20:38 -0400

    fixed and completed models

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,4 @@
-const Sequelize = requile("sequelize");
+const Sequelize = require("sequelize");
 
 require("dotenv").config();
 
--- models/index.js
+++ models/index.js
@@ -2,27 +2,29 @@ const User = require("./User");
 const Post = require("./Post");
 const Comment = require("./Comment");
 
-User.hasMany(Post, {
-  onDelete: "cascade",
-});
+const { belongsTo } = require("./User");
 
 Post.belongsTo(User, {
   foreignKey: "user_id",
 });
 
-Comment.belongsTo(User, {
-  foreignKey: "user_id",
+User.hasMany(Post, {
+  onDelete: "cascade",
 });
 
 Comment.belongsTo(Post, {
   foreignKey: "post_id",
 });
 
-User.hasMany(Comment, {
+Post.hasMany(Comment, {
   onDelete: "cascade",
 });
 
-Post.hasMany(Comment, {
+Comment.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+User.hasMany(Comment, {
   onDelete: "cascade",
 });
 
